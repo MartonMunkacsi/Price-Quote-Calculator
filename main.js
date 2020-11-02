abc6c579ffd022710d12d7a346cc7463652d132e
MartonMunkacsi <marton.munkacsi09@gmail.com>
2020-11-02 03:42:31 +0000

    1st Commit
    - added a cart icon
    - added a counter for cart icon
    - some css changes
    - new list/unordered list added to html

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let btn = document.getElementById("btn");
 
+//Price per square metre.
 let pricePsqm = 1.25; 
 
 
@@ -8,5 +9,16 @@ btn.addEventListener('click', function() {
     let customWidth = document.getElementById('banner_width').value;
 
     let totalBill = document.getElementById('total_bill').value = customLength * customWidth * pricePsqm;
+
+
+    document.getElementById('finalSum').innerHTML = "Your final bill will be £"+totalBill
   
 })
+//new additions. Following lines will add the new price quote to cart with measurements etc in detailed receipt. (1st Commit)
+let cartCount = document.getElementById("cart_count");
+
+
+addcart.addEventListener('click', function(){
+    //need to make 'addcart' button increment value of cart counter using i++
+})
+
